6b4f7780f3b02d599617dbf85cac573ea5d0e803
Peter & Frank Johnson <student@epicodus.com>
2015-04-23 09:47:02 -0700

    added second test which succeeds.

--- lib/coin_combinations.rb
+++ lib/coin_combinations.rb
@@ -2,10 +2,19 @@ class Fixnum
 
   define_method(:coin_combos) do
 
-  input = 2
+  input = self
+  penny = 1
+  nickel = 5
 
-  output = "penny x #{input}"
+
+  if input < 5
+    output = "penny x #{input}"
+  else
+    input > 10
+    output = "nickel x 1, penny x #{input - nickel}"
+  end
   output
+
   end
 
 end
