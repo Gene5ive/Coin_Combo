769a61d25727119a23e15e0f7568fcc0b531fd75
Peter & Frank Johnson <student@epicodus.com>
2015-04-23 11:40:19 -0700

    setup integrations

---
+++ spec/coin_combinations_integration_spec.rb
@@ -0,0 +1,12 @@
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+
+describe('the coin combo path', {:type => :feature}) do
+  it('processes the user change amount and returns least amount of coins') do
+    visit('/')
+    fill_in('change' :with => 78)
+    click_button('Cha-ching')
+    expect(page).to have_content('quarters x 3, dimes x 0, nickels x 0, pennies x 3')
+  end
+end
