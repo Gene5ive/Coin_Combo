e661018a8b84167864abe3fba3670747a32ad73a
Peter & Frank Johnson <student@epicodus.com>
2015-04-23 09:39:29 -0700

    added second test which fails.

--- spec/coin_combinations_spec.rb
+++ spec/coin_combinations_spec.rb
@@ -2,8 +2,11 @@ require('rspec')
 require('coin_combinations')
 
 describe('sorting coins path')do
-  it('gives you minimum amount coins required from the user input') do
-  expect((2).coin_combos()).to(eq("penny x 2"))
+  it('gives you minimum amount of pennies required from the user input') do
+    expect((2).coin_combos()).to(eq("penny x 2"))
+  end
 
+  it('gives you minimum amount of pennies and nickels from the user input') do
+    expect((7).coin_combos()).to(eq("nickel x 1, penny x 2"))
   end
 end
